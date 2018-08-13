8f2eae6fbc3a54cf15279e0ee7e46381a4f9655a
Quang Nguyen <quang.nguyen@mgm-tp.com>
2018-08-14 01:05:54 +0700

    Create EmployeeNode with proper json output format

---
+++ app/EmployeeNode.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qnguyen
+ * Date: 8/13/18
+ * Time: 2:36 PM
+ */
+
+namespace App;
+
+
+use JsonSerializable;
+
+/**
+ * Class EmployeeNode represents a node in a employee hierarchy tree
+ */
+class EmployeeNode implements JsonSerializable {
+
+    private $employeeName;
+    private $subordinates;
+
+    public function __construct($employeeName) {
+        $this->employeeName = $employeeName;
+        $this->subordinates = [];
+    }
+
+    public function addSubordinate($employeeNode) {
+        $this->subordinates[] = $employeeNode;
+    }
+
+    public function getEmployeeName() {
+        return $this->employeeName;
+    }
+
+    public function getSubordinates(): array {
+        return $this->subordinates;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON.
+     * The json output would have key as employeeName and value is its subordinates
+     */
+    public function jsonSerialize() {
+        return [
+            $this->employeeName => $this->subordinates
+        ];
+    }
+}
