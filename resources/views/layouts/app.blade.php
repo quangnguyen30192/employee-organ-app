80514a7730e2db944687dfbc459148fcdcf07c78
Quang Nguyen <quang.nguyen@mgm-tp.com>
2018-08-14 17:58:30 +0700

    Create IndexController and add starter bootstrap landing page, layout page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Employee Hierarchy App</title>
+
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    @yield('style')
+
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="{{route('index')}}">
+                        Employee Organizer
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Upload Json File</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+<!-- jQuery -->
+<script src="{{asset('js/app.js')}}"></script>
+<script>
+        $("#wrapper").toggleClass("toggled");
+</script>
+
+@yield('script')
+
+</body>
+
+</html>
