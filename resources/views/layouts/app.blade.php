34f4d2b53be76fca2df1838c92fc8a665051d0fd
Quang Nguyen <quang.nguyen@mgm-tp.com>
2018-08-25 14:04:41 +0700

    Add crsf meta tag to dismiss the crsf error in console log

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
 
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Employee Hierarchy App</title>
 
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
